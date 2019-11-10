5f097e694b7dd4343ab0218fbd949306b846c7d0
Administrator <849183239@qq.com>
2019-11-10 19:36:45 +0800

    springcloudbus
    springboot 2.2.0 与 mybatis 不兼容
    改为 2.2.1

--- order-server/src/main/java/com/studycloud/orderserver/controller/EnvController.java
+++ order-server/src/main/java/com/studycloud/orderserver/controller/EnvController.java
@@ -3,18 +3,21 @@ package com.studycloud.orderserver.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/env")
 @RestController
+@RefreshScope
 public class EnvController {
 
     @Value("${env}")
     private  String env;
 
-    @GetMapping("getValue")
+    @GetMapping("/getValue")
     public String getEnv(){
         return env;
     }
